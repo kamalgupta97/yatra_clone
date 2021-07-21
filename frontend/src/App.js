29760a668796500efd8e62df13f5a762e37b42f7
Bhargavi <bhargavishetty113@gmail.com>
2021-07-21 12:02:23 +0530

    Bus selection page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
-import { SeatSelection } from './Pages/SeatSelection/SeatSelection';
+import "./App.css";
+import { SeatSelection } from "./Pages/SeatSelection/SeatSelection";
+import Routes from "./Routes/Routes";
 
 function App() {
   return (
     <div className="App">
-      <SeatSelection/>
+      <SeatSelection />
+      <Routes />
     </div>
   );
 }

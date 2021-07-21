b49de77b67d52abe64e82f7d24c3c0c00adb9cd2
Mangeshbk <mangeshbkwsu@gmail.com>
2021-07-21 17:04:02 +0530

    homePage

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import { Buses } from "../Styles/BussesLandingPageStyle";
+import { Box, Container } from "@material-ui/core";
+import SearchCard from "../Components/Buses/SearchCard";
 
 export default function LandingPage() {
   return (
     <div>
       <Navbar />
+      <Buses>
+        <Container>
+          <Box>
+            <SearchCard />
+          </Box>
+          <Box></Box>
+        </Container>
+      </Buses>
     </div>
   );
 }
---
+++ frontend/src/Styles/BussesLandingPageStyle.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Buses = styled.div`
+  background-color: #fdfbfb;
+`;

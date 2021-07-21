1ce76d06a11f024359edf3d85bf9a9155ae09f9c
kamal <kamaldro97@gmail.com>
2021-07-21 10:18:17 +0530

    seatselection page commence

---
+++ frontend/src/Pages/SeatSelection/SeatSelection.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+
+import styles from  './SeatSelection.module.css';
+
+
+export const SeatSelection = () => {
+    return (
+        <div className={styles.wrapper}>
+            <h1>SeatSelection</h1>
+        </div>
+    )
+}

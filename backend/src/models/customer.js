49aabd3dbe48e56cfe47211077901c418f2ccf7c
Mangeshbk <mangeshbkwsu@gmail.com>
2021-07-23 05:55:24 +0530

    customers schema added

---
+++ backend/src/models/customer.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const customerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  //   googleId: {
+  //     type: String,
+  //     required: false,
+  //   },
+  age: {
+    type: Number,
+    required: false,
+  },
+  gender: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: String,
+    required: false,
+  },
+  profilePicture: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Customers", customerSchema);

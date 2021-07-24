ab8685458fae1337c910914f1e738b8751dc9347
Mangeshbk <mangeshbkwsu@gmail.com>
2021-07-25 02:46:54 +0530

    backend v1 started

--- backend/src/models/bus.js
+++ backend/src/models/bus.js
@@ -1,48 +1,51 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const busSchema = new Schema({
-  operatorName: {
-    type: String,
-    required: true,
+const busSchema = new Schema(
+  {
+    busType: {
+      type: String,
+      required: true,
+    },
+    departureTime: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    totalSeats: {
+      type: Number,
+      required: false,
+    },
+    routes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Routes",
+        required: true,
+      },
+    ],
+    liveTracking: {
+      type: Number,
+      required: true,
+    },
+    fare: {
+      type: Number,
+      required: false,
+    },
+    busServices: {
+      ac: { type: Boolean, default: false },
+      wifi: { type: Boolean, default: false },
+      cctv: { type: Boolean, default: false },
+      waterBottles: { type: Boolean, default: false },
+      vaccinatedStaff: { type: Boolean, default: false },
+      primo: { type: Boolean, default: false },
+    },
   },
-  busType: {
-    type: String,
-    required: true,
-  },
-  seatnumber: { 
-    type: [Number],
-    required: true
-  },
-  departureTime: {
-    type: String,
-    required: true,
-  },
-  rating: {
-    type: [Number],
-    required: false,
-  },
-  totalSeats: {
-    type: Number,
-    required: false,
-  },
-  routes: {
-    type: Schema.Types.ObjectId,
-    ref: "Routes",
-    required: true,
-  },
-  images: {
-    type: String,
-    required: true,
-  },
-  liveTracking: {
-    type: Number,
-    required: false,
-  },
-  busServices: {
-    type: Schema.Types.ObjectId,
-    ref: "BusServices",
-    required: true,
-  },
-});
-//date
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
 module.exports = mongoose.model("Buses", busSchema);

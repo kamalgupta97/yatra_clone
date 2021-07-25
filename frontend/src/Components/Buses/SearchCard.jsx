c253ebace43cdcf4a3b66d4e04420dbb464b0bbb
Mangeshbk <mangeshbkwsu@gmail.com>
2021-07-25 10:44:21 +0530

    backend

--- frontend/src/Components/Buses/SearchCard.jsx
+++ frontend/src/Components/Buses/SearchCard.jsx
@@ -70,7 +70,7 @@ export default function SearchCard() {
     const payload = {
       departureLocation,
       arrivalLocation,
-      selectedDate,
+      selectedDate: selectedDate,
     };
 
     setSearchData(payload);
